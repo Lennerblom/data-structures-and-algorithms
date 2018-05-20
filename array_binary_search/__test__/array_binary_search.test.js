7e66fcf84f8f09055529335a445768f27b0e24c6
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-05-20 16:47:22 -0700

    3 working tests including large array test

--- array_binary_search/__test__/array_binary_search.test.js
+++ array_binary_search/__test__/array_binary_search.test.js
@@ -1,12 +1,27 @@
 'use strict';
 
-const BinarySearch = require('../lib/array_binary.js');
+const BinarySearch = require('../lib/array_binary_search.js');
 
 describe('BinarySearch', () => {
 
   it('BinarySearch() returns index of given number', () => {
-
     expect(BinarySearch([4,8,15,16,32],16)).toBe(3);
-    
+  });
+
+  it('BinarySearch() returns -1 if search key does not exist in array', () => {
+    expect(BinarySearch([4,8,15,16,32],7)).toBe(-1);
+  });
+
+  it('BinarySearch works with large arrays', () => {
+    function makeBigArray() {
+      const bigArr = [];
+      for(let i = 0; i < 10000; i++) {
+        bigArr.push(i);
+      }
+      return bigArr;
+    }
+    makeBigArray();
+
+    expect(BinarySearch(makeBigArray(),4459)).toBe(4459);
   });
 });
