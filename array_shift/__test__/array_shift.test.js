666b16b87fda489468c420416a41a0644a8d8ee2
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-05-20 15:05:55 -0700

    began initial code, still debugging

---
+++ array_shift/__test__/array_shift.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const insertShiftArray = require('../lib/array_shift.js');
+
+describe('insertShiftArray', () => {
+    
+  it('push() returns a given array and inserts a given input to place in the middle of the array ', () => {
+    let arr = [1,2,3,4];
+    let num = 14;
+    insertShiftArray(arr,num);
+
+    expect(arr).toBe([1,2,3,14,4]);
+  });
+});
