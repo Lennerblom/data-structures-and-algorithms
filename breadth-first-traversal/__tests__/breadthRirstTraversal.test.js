32ea70a823c1503bdb0fb88edaf6090917d00918
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-07-23 13:51:09 -0700

    added bft and the beginning of a test, bft does not include the left nodes for some reason

--- breadth-first-traversal/__tests__/breadthRirstTraversal.test.js
+++ breadth-first-traversal/__tests__/breadthRirstTraversal.test.js
@@ -1,1 +1,23 @@
 'use strict';
+
+import Tree from '../../trees/lib/trees.js';
+
+import bft from '../lib/breadthFirstTraversal.js';
+
+describe('Breadth First Traversal', () => {
+  let mytree = new Tree;
+  mytree.insert(5);
+  mytree.insert(6);
+  mytree.insert(7);
+  mytree.insert(3);
+  mytree.insert(1);
+  mytree.insert(11);
+
+
+  it('should return something', () => {
+    let actual, expected;
+    actual = bft(mytree);
+    expected = 'something';
+    expect(actual).toBe(expected);
+  });
+});
--- breadth-first-traversal/lib/breadthFirstTraversal.js
+++ breadth-first-traversal/lib/breadthFirstTraversal.js
@@ -1,50 +1,25 @@
 'use strict';
 
-class Node {
-    constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+import Tree from '../../trees/lib/trees.js';
+
+function breadthFirstTraversal(tree) {
+  if(!tree.root === null) {
+    return;
+  }
+  const queue = [];
+  queue.push(tree.root);
+
+  while(queue.length > 0) {
+    let current = queue[0];
+    if(current.left !== null) {
+      queue.push(current.left);
     }
-}
-
-class BinaryTree {
-    constructor() {
-        this.root = null;
-        this.children = [];
+    if(current.right !== null) {
+      queue.push(current.right);
     }
-insert(node){
-    if(!this.root){
-        this.root = new Node(node);
-        return this.root;
-    } 
-    let current = root;
-    let newNode = new Node(node);
-
-    while(current){
-      if(node < current.value){
-        if(!current.left){
-              current.left = newNode;
-              break;
-            }
-        else {
-            current = current.left;
-        }
-    } else {
-        if(!current.right){
-            current.right = newNode;
-            break;
-        } else {
-            current = current.right;
-        }
-    }
-}
-}
-}
-let breadthFirstTraversal(tree) {
-
+    console.log(queue.shift().value);
+    queue.shift();
+  }  
 }
 
-
-// Feature Tasks
-// Write a function called breadthFirstTraversal which takes a Binary Tree as its unique input. Without utilizing any of the built-in methods available to your language, traverse the input tree using a Breadth-first approach; print every visited node’s value.
+export default breadthFirstTraversal;
